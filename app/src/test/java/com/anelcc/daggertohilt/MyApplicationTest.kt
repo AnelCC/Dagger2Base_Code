fb97845699d2b18eb467dd73a593d62783768648
Anel CC <anel.calvo.cervantes@gmail.com>
2020-08-13 01:42:35 -0400

    [Dagger_To_Hilt_01] 8. Add testing

--- app/src/test/java/com/anelcc/daggertohilt/MyApplicationTest.kt
+++ app/src/test/java/com/anelcc/daggertohilt/MyApplicationTest.kt
@@ -1,13 +1,69 @@
 package com.anelcc.daggertohilt
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import com.anelcc.daggertohilt.di.StorageModule
+import com.anelcc.daggertohilt.main.MainActivity
+import org.junit.Rule
+import org.junit.Test
+
 
 /**
  * MyTestApplication will override MyApplication in android tests
  */
-class MyApplicationTest : MyApplication() {
+@UninstallModules(StorageModule::class)
+@HiltAndroidTest
+class MyApplicationTest {
 
-    /*override fun initializeComponent(): AppComponent {
-        // Creates a new TestAppComponent that injects fakes types
-        return DaggerTestAppComponent.create()
-    }*/
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Test
+    fun runApp() {
+        ActivityScenario.launch(MainActivity::class.java)
+
+        // Should be in Registration/EnterDetails because the user is not registered
+        onView(withText("Register to Dagger World!")).check(matches(isDisplayed()))
+        onView(withId(R.id.username)).perform(typeText("username"), closeSoftKeyboard())
+        onView(withId(R.id.password)).perform(typeText("password"), closeSoftKeyboard())
+        onView(withId(R.id.next)).perform(click())
+
+        // Registration/T&Cs
+        onView(withText("Terms and Conditions")).check(matches(isDisplayed()))
+        onView(withText("REGISTER")).perform(click())
+
+        // Main
+        onView(withText("Hello username!")).check(matches(isDisplayed()))
+        onView(withText("SETTINGS")).perform(click())
+
+        // Settings
+        onView(withText("REFRESH NOTIFICATIONS")).check(matches(isDisplayed()))
+        onView(withText("LOGOUT")).perform(click())
+
+        // Login
+        onView(withText("Welcome to Dagger World!")).check(matches(isDisplayed()))
+        onView(withId(R.id.password)).perform(typeText("password"), closeSoftKeyboard())
+        onView(withText("LOGIN")).perform(click())
+
+        // Main
+        onView(withText("Hello username!")).check(matches(isDisplayed()))
+        onView(withText("SETTINGS")).perform(click())
+
+        // Settings
+        onView(withText("REFRESH NOTIFICATIONS")).check(matches(isDisplayed()))
+        onView(withText("LOGOUT")).perform(click())
+
+        // Login
+        onView(withText("Welcome to Dagger World!")).check(matches(isDisplayed()))
+        onView(withId(R.id.unregister)).perform(click())
+
+        // Registration
+        onView(withText("Register to Dagger World!")).check(matches(isDisplayed()))
+    }
 }
--- app/src/test/java/com/anelcc/daggertohilt/di/StorageModuleTest.kt
+++ app/src/test/java/com/anelcc/daggertohilt/di/StorageModuleTest.kt
@@ -4,15 +4,14 @@ import com.anelcc.daggertohilt.storage.SharedPreferencesStorageTest
 import com.anelcc.daggertohilt.storage.Storage
 import dagger.Binds
 import dagger.Module
-import org.junit.Test
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
 
-import org.junit.Assert.*
 
 // Overrides StorageModule in android tests
+@InstallIn(ApplicationComponent::class)
 @Module
 abstract class StorageModuleTest {
-
-
     // Makes Dagger provide FakeStorage when a Storage type is requested
     @Binds
     abstract fun provideStorage(storage: SharedPreferencesStorageTest): Storage
