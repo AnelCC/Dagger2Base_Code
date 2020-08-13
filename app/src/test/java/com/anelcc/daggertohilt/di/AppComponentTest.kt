cc97eb9673a9a210886297d3841b680de63d49c0
Anel CC <anel.calvo.cervantes@gmail.com>
2020-08-12 22:26:13 -0400

    [Dagger007_TestingDagger] Testing supports Dagger

---
+++ app/src/test/java/com/anelcc/daggertohilt/di/AppComponentTest.kt
@@ -0,0 +1,15 @@
+package com.anelcc.daggertohilt.di
+
+import dagger.Binds
+import dagger.Component
+import dagger.Module
+import org.junit.Test
+
+import org.junit.Assert.*
+import javax.inject.Singleton
+
+// Replacement for AppComponent in android tests
+@Singleton
+// Includes TestStorageModule that overrides objects provided in StorageModule
+@Component(modules = [StorageModuleTest::class, AppSubcomponents::class])
+interface AppComponentTest : AppComponent
